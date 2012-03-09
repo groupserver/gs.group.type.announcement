ce00b849636c23c43cfd3c95d62302c7c71db9fd
Michael JasonSmith <mpj17@onlinegroups.net>
2012-03-09 15:53:13 +1100

    Adding the No Group Properties can-post viewlet

---
+++ gs/group/type/announcement/canpostviewlets.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+from zope.cachedescriptors.property import Lazy
+from gs.group.member.canpost import RuleViewlet
+from canpostrules import PostingMember
+
+class NotAPostingMember(RuleViewlet):
+    weight = PostingMember.weight
+    
+    @Lazy
+    def show(self):
+        retval = self.canPost.statusNum == self.weight
+        assert type(retval) == bool
+        return retval
+
