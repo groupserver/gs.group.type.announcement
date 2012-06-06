7d8db004098da0143ea1dc850a1f2b6c5b6a9c50
Michael JasonSmith <mpj17@onlinegroups.net>
2012-06-06 14:35:11 +1000

    Switching to the PublicTab for the base-type of the Post and Files tabs.

--- gs/group/type/announcement/tabs.py
+++ gs/group/type/announcement/tabs.py
@@ -3,19 +3,19 @@ from zope.cachedescriptors.property import Lazy
 from zope.component import getMultiAdapter
 from Products.GSGroup.interfaces import IGSMailingListInfo
 from gs.group.member.canpost.interfaces import IGSPostingUser
-from gs.group.home.simpletab import UserInfoTab
+from gs.group.home.simpletab import PublicTab
 
-class DoNothing(UserInfoTab):
+class DoNothing(PublicTab):
     def __init__(self, group, request, view, manager):
-        UserInfoTab.__init__(self, group, request, view, manager)
+        PublicTab.__init__(self, group, request, view, manager)
             
     @Lazy
     def show(self):
         return False
 
-class PostsTab(UserInfoTab):
+class PostsTab(PublicTab):
     def __init__(self, group, request, view, manager):
-        UserInfoTab.__init__(self, group, request, view, manager)
+       PublicTab.__init__(self, group, request, view, manager)
         
     @Lazy
     def canPost(self):
