94e2a4af37c1a0c97b0a9382fe03c4576f3abddc
Michael JasonSmith <mpj17@onlinegroups.net>
2013-02-28 14:40:37 +1100

    Changing the base classes for the tabs.

--- gs/group/type/announcement/tabs.py
+++ gs/group/type/announcement/tabs.py
@@ -1,24 +1,25 @@
 # coding=utf-8
 from zope.cachedescriptors.property import Lazy
 from zope.component import getMultiAdapter
-from Products.GSGroup.interfaces import IGSMailingListInfo
 from gs.group.member.canpost.interfaces import IGSPostingUser
-from gs.group.home.simpletab import PublicTab
+from gs.group.home.simpletab import PublicTab, UserInfoTab
+
 
 class DoNothing(PublicTab):
     def __init__(self, group, request, view, manager):
-        PublicTab.__init__(self, group, request, view, manager)
-            
+        super(DoNothing, self).__init__(group, request, view, manager)
+
     @Lazy
     def show(self):
         return False
 
-class PostsTab(PublicTab):
+
+class PostsTab(UserInfoTab):
     def __init__(self, group, request, view, manager):
-       PublicTab.__init__(self, group, request, view, manager)
-        
+        super(PostsTab, self).__init__(group, request, view, manager)
+
     @Lazy
     def canPost(self):
-        retval = getMultiAdapter((self.groupInfo.groupObj, self.userInfo), 
-                                  IGSPostingUser)
+        retval = getMultiAdapter((self.groupInfo.groupObj, self.loggedInUser),
+                                    IGSPostingUser)
         return retval
