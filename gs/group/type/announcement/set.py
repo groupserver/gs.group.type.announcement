0ad9f65896055f99cfe7a1e756ef72704178d545
Michael JasonSmith <mpj17@onlinegroups.net>
2014-07-17 15:06:44 -0700

    Adding the Set Announcment Group adaptor

---
+++ gs/group/type/announcement/set.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+############################################################################
+#
+# Copyright © 2014 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+############################################################################
+from __future__ import absolute_import, unicode_literals
+from gs.group.type.set import SetABC
+
+
+class SetAnnouncementGroup(SetABC):
+    'Set a group folder to be an announcement group'
+    name = 'Announcement group'
+    typeId = 'gs-group-type-announcement'
+    weight = 20
+    show = True
+
+    def set(self):
+        '''Add the marker-interface to make the group into a discussion
+        group.'''
+        iFaces = [
+            'gs.group.type.announcement.interfaces.IGSAnnouncementGroup']
+        self.add_marker_interfaces(self.group, iFaces)
+
+        # TODO: Reply To
