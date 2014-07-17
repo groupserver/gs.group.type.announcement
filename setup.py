1dc083fe1d14a837396a4a1124295d5b32772af5
Michael JasonSmith <mpj17@onlinegroups.net>
2014-07-17 15:06:31 -0700

    PEP-8 OCD

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-##############################################################################
+############################################################################
 #
 # Copyright © 2013, 2014 OnlineGroups.net and Contributors.
 # All Rights Reserved.
@@ -11,7 +11,7 @@
 # WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
 # FOR A PARTICULAR PURPOSE.
 #
-##############################################################################
+############################################################################
 import codecs
 import os
 from setuptools import setup, find_packages
@@ -21,52 +21,55 @@ version = get_version()
 
 with codecs.open('README.txt', encoding='utf-8') as f:
     long_description = f.read()
-with codecs.open(os.path.join("docs", "HISTORY.txt"), encoding='utf-8') as f:
+with codecs.open(os.path.join("docs", "HISTORY.txt"),
+                 encoding='utf-8') as f:
     long_description += '\n' + f.read()
 
-setup(name='gs.group.type.announcement',
-    version=version,
-    description="Support for announcement groups in GroupServer",
-    long_description=long_description,
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        "Environment :: Web Environment",
-        "Framework :: Zope2",
-        "Intended Audience :: Developers",
-        'License :: OSI Approved :: Zope Public License',
-        "Natural Language :: English",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    keywords='group, announcement, posts, topics, canpost',
-    author='Michael JasonSmith',
-    author_email='mpj17@onlinegroups.net',
-    url='https://source.iopen.net/groupserver/gs.group.type.announcement/',
-    license='ZPL 2.1',
-    packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['gs', 'gs.group', 'gs.group.type', ],
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        'setuptools',
-        'zope.cachedescriptors',
-        'zope.component',
-        'zope.browserpage',
-        'zope.browserresource',
-        'zope.tal',
-        'zope.tales',
-        'zope.viewlet',
-        'gs.content.js.jquery.base',
-        'gs.group.home',
-        'gs.group.member.canpost',
-        'gs.group.member.list',
-        'gs.group.member.viewlet',
-        'gs.group.messages.base',
-        'gs.group.type.discussion',
-        'Products.GSGroup',
-    ],
-    entry_points="""
-    # -*- Entry points: -*-
-    """,
-)
+name = 'gs.group.type.announcement'
+url = 'https://source.iopen.net/groupserver/{0}/'.format(name)
+
+setup(name=name,
+      version=version,
+      description="Support for announcement groups in GroupServer",
+      long_description=long_description,
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          "Environment :: Web Environment",
+          "Framework :: Zope2",
+          "Intended Audience :: Developers",
+          'License :: OSI Approved :: Zope Public License',
+          "Natural Language :: English",
+          "Operating System :: POSIX :: Linux",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      keywords='group, announcement, posts, topics, canpost',
+      author='Michael JasonSmith',
+      author_email='mpj17@onlinegroups.net',
+      url=url,
+      license='ZPL 2.1',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['gs', 'gs.group', 'gs.group.type', ],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'zope.cachedescriptors',
+          'zope.component',
+          'zope.browserpage',
+          'zope.browserresource',
+          'zope.tal',
+          'zope.tales',
+          'zope.viewlet',
+          'gs.content.js.jquery.base',
+          'gs.group.home',
+          'gs.group.member.canpost',
+          'gs.group.member.list',
+          'gs.group.member.viewlet',
+          'gs.group.messages.base',
+          'gs.group.type.discussion',
+          'Products.GSGroup',
+      ],
+      entry_points="""
+          # -*- Entry points: -*-
+      """,)
