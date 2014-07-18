73e7444478856639b488b03194dfd34292827f46
Michael JasonSmith <mpj17@onlinegroups.net>
2014-07-17 17:34:19 -0700

    Handling Reply-to, I hope.

--- gs/group/type/announcement/set.py
+++ gs/group/type/announcement/set.py
@@ -13,21 +13,53 @@
 #
 ############################################################################
 from __future__ import absolute_import, unicode_literals
-from gs.group.type.set import SetABC
+from gs.group.type.set import (SetABC, UnsetABC)
 
 
 class SetAnnouncementGroup(SetABC):
     'Set a group folder to be an announcement group'
     name = 'Announcement group'
-    typeId = 'gs-group-type-announcement'
+    typeId = 'gs-group-type-announcement-set'
     weight = 20
     show = True
 
     def set(self):
+        self.set_marker()
+        self.set_replyto()
+
+    def set_marker(self):
         '''Add the marker-interface to make the group into a discussion
         group.'''
         iFaces = [
             'gs.group.type.announcement.interfaces.IGSAnnouncementGroup']
-        self.add_marker_interfaces(self.group, iFaces)
+        self.add_marker(self.group, iFaces)
 
-        # TODO: Reply To
+    def set_replyto(self):
+        'Set the reply-to property of the mailing list'
+        siteRoot = self.group.site_root()
+        listManager = getattr(siteRoot, 'ListManager')
+        mailingList = getattr(listManager, self.group.getId())
+        if mailingList.hasProperty('replyto'):
+            mailingList.manage_changeProperties(replyto='sender')
+        else:
+            mailingList.manage_addProperty('replyto', 'sender', 'string')
+
+
+class UnsetAnnouncementGroup(UnsetABC):
+    name = 'Announcement group'
+
+    def unset(self):
+        self.unset_marker()
+        self.unset_replyto()
+
+    def unset_marker(self):
+        iFaces = [
+            'gs.group.type.announcement.interfaces.IGSAnnouncementGroup']
+        self.del_marker(self.group, iFaces)
+
+    def unset_replyto(self):
+        siteRoot = self.group.site_root()
+        listManager = getattr(siteRoot, 'ListManager')
+        mailingList = getattr(listManager, self.group.getId())
+        if mailingList.hasProperty('replyto'):
+            mailingList.manage_delProperties(['replyto', ])
