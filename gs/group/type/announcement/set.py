6124e184fba6e0f8fcbcf678259848f0d3775871
Michael JasonSmith <mpj17@onlinegroups.net>
2014-07-18 15:59:29 -0700

    Moving the list-property methods to the ABC.

--- gs/group/type/announcement/set.py
+++ gs/group/type/announcement/set.py
@@ -24,7 +24,7 @@ class SetAnnouncementGroup(SetABC):
 
     def set(self):
         self.set_marker()
-        self.set_replyto()
+        self.set_list_property('replyto', 'sender')
 
     def set_marker(self):
         '''Add the marker-interface to make the group into a discussion
@@ -33,16 +33,6 @@ class SetAnnouncementGroup(SetABC):
             'gs.group.type.announcement.interfaces.IGSAnnouncementGroup']
         self.add_marker(self.group, iFaces)
 
-    def set_replyto(self):
-        'Set the reply-to property of the mailing list'
-        siteRoot = self.group.site_root()
-        listManager = getattr(siteRoot, 'ListManager')
-        mailingList = getattr(listManager, self.group.getId())
-        if mailingList.hasProperty('replyto'):
-            mailingList.manage_changeProperties(replyto='sender')
-        else:
-            mailingList.manage_addProperty('replyto', 'sender', 'string')
-
 
 class UnsetAnnouncementGroup(UnsetABC):
     name = 'Announcement group'
@@ -50,16 +40,9 @@ class UnsetAnnouncementGroup(UnsetABC):
 
     def unset(self):
         self.unset_marker()
-        self.unset_replyto()
+        self.del_list_property('replyto')
 
     def unset_marker(self):
         iFaces = [
             'gs.group.type.announcement.interfaces.IGSAnnouncementGroup']
         self.del_marker(self.group, iFaces)
-
-    def unset_replyto(self):
-        siteRoot = self.group.site_root()
-        listManager = getattr(siteRoot, 'ListManager')
-        mailingList = getattr(listManager, self.group.getId())
-        if mailingList.hasProperty('replyto'):
-            mailingList.manage_delProperties(['replyto', ])
