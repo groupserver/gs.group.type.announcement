f404967ea1dc3d45e7908304b9b95c7eb7290637
Michael JasonSmith <mpj17@onlinegroups.net>
2016-02-01 11:07:47 -0800

    Mostly whitespace changes to make gjslint happy

--- gs/group/type/announcement/browser/javascript/posts.js
+++ gs/group/type/announcement/browser/javascript/posts.js
@@ -1,7 +1,7 @@
-"use strict";
+'use strict';
 // Posts list for an announcement group
 //
-// Copyright © 2013, 2014 OnlineGroups.net and Contributors.
+// Copyright © 2013, 2014, 2016 OnlineGroups.net and Contributors.
 // All Rights Reserved.
 //
 // This software is subject to the provisions of the Zope Public License,
@@ -10,19 +10,19 @@ jQuery.noConflict();
 
 function gs_group_type_announcement_posts() {
     // HACK to get the group ID for the AJAX.
-    var bodyId=null, gId=null, e=/_.*$/,  postsSearch=null;
+    var bodyId = null, gId = null, e = /_.*$/, postsSearch = null;
 
     bodyId = jQuery('body').attr('id');
     gId = String(e.exec(bodyId)).slice(1);
 
     postsSearch = GSSearch('#gs-group-type-announcement-posts',
-                           '/s/search.ajax', 0, 6, 
+                           '/s/search.ajax', 0, 6,
                            {'t': '0', 'p': '1', 'g': gId}, null);
     postsSearch.load();
     jQuery('#gs-group-messages-base-tabs-buttons li:first-child a').click();
 }
 
-jQuery(document).ready( function () {
-    gsJsLoader.with_module('/++resource++gs-search-base-js-min-20140313.js', 
+jQuery(document).ready(function() {
+    gsJsLoader.with_module('/++resource++gs-search-base-js-min-20140313.js',
                            gs_group_type_announcement_posts);
 });
