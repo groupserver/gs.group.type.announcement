82f6567e74918b93d0e9b4b43c03546fbd03b401
Michael JasonSmith <mpj17@onlinegroups.net>
2016-02-01 11:09:56 -0800

    Minifying, and using the new code.

--- gs/group/type/announcement/browser/javascript/posts-min.js
+++ gs/group/type/announcement/browser/javascript/posts-min.js
@@ -1,4 +1,1 @@
-"use strict";jQuery.noConflict();function gs_group_type_announcement_posts(){var b=null,a=null,c=/_.*$/,d=null;
-b=jQuery("body").attr("id");a=String(c.exec(b)).slice(1);d=GSSearch("#gs-group-type-announcement-posts","/s/search.ajax",0,6,{t:"0",p:"1",g:a},null);
-d.load();jQuery("#gs-group-messages-base-tabs-buttons li:first-child a").click()}jQuery(document).ready(function(){gsJsLoader.with_module("/++resource++gs-search-base-js-min-20140313.js",gs_group_type_announcement_posts)
-});
+'use strict';jQuery.noConflict();function gs_group_type_announcement_posts(){var a=null,a=a=null,a=jQuery("body").attr("id"),a=String(/_.*$/.exec(a)).slice(1),a=GSSearch("#gs-group-type-announcement-posts","/s/search.ajax",0,6,{t:"0",p:"1",g:a},null);a.load();jQuery("#gs-group-messages-base-tabs-buttons li:first-child a").click()}jQuery(document).ready(function(){gsJsLoader.with_module("/++resource++gs-search-base-js-min-20140313.js",gs_group_type_announcement_posts)});
